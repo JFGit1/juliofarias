76235901382a5985dce8c0ec3329c28f049ad2bb
JF Git <juliofarias+git1@gmail.com>
2023-06-28 04:20:03 -0300

    First commit - Installation and cleaning 0.2

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,12 +8,14 @@ export default function Home() {
 				desc='UX/UI Designer - Web Developer - Motion Designer'
 			/>
 			<main className='container mx-auto h-screen flex items-center flex-col justify-center'>
-				<h1 className='font-bold text-6xl text-white mt-[-14rem] mb-2'>
-					JULIO FARIAS
-				</h1>
-				<p className='text-white text-2xl font-normal'>
-					UX/UI Designer - Web Developer - Motion Designer
-				</p>
+				<div className='flex justify-center flex-col items-center mt-[-16rem] px-4 py-2 rounded-lg bg-[#0f1e7293] border border-[#2541e2b9]'>
+					<h1 className='font-bold text-3xl text-[#00b7ff] mb-1'>
+						JULIO FARIAS.com
+					</h1>
+					<p className='text-white text-lg font-normal'>
+						UX/UI Designer - Web Developer - Motion Designer
+					</p>
+				</div>
 			</main>
 		</>
 	);
